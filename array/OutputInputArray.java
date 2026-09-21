bf85e31ad226de7ccee883646ff3be54a1cbe199
KUSHAL P <kushalmys55@gmail.com>
2026-09-21 20:58:00 +0530

    Initial commit

---
+++ array/OutputInputArray.java
@@ -0,0 +1,47 @@
+package array;
+
+public class OutputInputArray{
+    public static void main (String[] args){
+        // int arr[] = {4,39,8,32,34,34};
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i] + " ");
+        // }
+
+ //default value of int array is 0
+    //    int arr[]=new int[6];
+    //     for(int i=0;i<arr.length;i++){
+    //         System.out.print(arr[i] + " ");
+    //     }
+     
+// input and output of array
+    // int arr[]=new int[8];
+    // Scanner sc=new Scanner(System.in);
+    // for(int i=0;i<arr.length;i++){
+    //     arr[i]=sc.nextInt();
+    // }
+    // for(int i=0;i<arr.length;i++){
+    //     System.out.print(arr[i] + " ");
+    // }
+    
+ // print negative numbers from array
+        // int arr[]={32,-54,-89,-98,83,83};
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]<0){
+        //         System.out.println(arr[i] + " ");
+        //     }
+        
+        // }
+
+// print sum of elements of the array
+        // Scanner sc=new Scanner(System.in);
+        // int arr[]=new int[10];
+        // for(int i=0;i<arr.length;i++){
+        //     arr[i]=sc.nextInt();
+        // }
+        // int sum=0;
+        // for(int i=0;i<arr.length;i++){
+        //     sum=sum+arr[i];
+        // }
+        // System.out.println("Sum of elements: " + sum);
+    }
+}
---
+++ array/arraybasics.java
@@ -0,0 +1,17 @@
+package array;
+
+public class arraybasics {
+    public static void main(String[] args){
+        // int[] arr = {9,5,4,2,4,5};
+        // System.out.println(arr[3]);
+        // arr[0]=121;
+        // System.out.println(arr[0]);
+        int[] arr=new int [6];  // index size 0 to 5
+        arr[0]=30;
+        arr[1]=40;  
+        arr[2]=50;
+        arr[3]=60;
+        arr[4]=70;
+        arr[5]=80;
+    }
+}
